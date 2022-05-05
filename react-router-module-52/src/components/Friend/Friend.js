7ed17f247520df9e652b52d19ab0a3cfa1e4d182
Hamed Hasan <71956267+Hamed-Hasan@users.noreply.github.com>
2022-05-06 02:44:05 +0300

    react-router-module-52

---
+++ react-router-module-52/src/components/Friend/Friend.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Friend = (props) => {
+    const {name, username, id} = props.friend;
+
+    const navigate = useNavigate();
+
+    const showFriendDetail = () =>{
+        // navigate('/friends/'+id);
+        navigate(`/myfriends/${id}`)
+    }
+ 
+    return (
+        <div>
+            <h2>Name: {name}</h2>
+            <Link to={'/myfriends/'+ id}>show Detail</Link>
+            <button onClick={showFriendDetail}>{username} id:{id}</button>
+           
+        </div>
+    );
+};
+
+export default Friend;
