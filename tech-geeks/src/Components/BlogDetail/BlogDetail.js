13c330822b930e8d45a2a9d492e1c2e4c8d5ac4a
Hamed Hasan <71956267+Hamed-Hasan@users.noreply.github.com>
2022-05-06 02:53:35 +0300

    tech-geeks

---
+++ tech-geeks/src/Components/BlogDetail/BlogDetail.js
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react';
+import {BlogContext} from '../../App'
+const BlogDetail = () => {
+
+    const [blogs, setBlogs] = useContext(BlogContext)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BlogDetail;
